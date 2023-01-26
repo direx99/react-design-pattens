f74bc1c22defcc8f13aae2f02ec01343de478ef2
Dinith Hasaranga <dinithhasaranga@Diniths-MacBook-Air.local>
2023-01-26 19:45:20 +0530

    Split-screen components improved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import SplitScreen from './SplitScreen';
 
-const LeftHandComponent=()=>{
-  return <h1 style={{backgroundColor:"green"}}>Left</h1>;
+const LeftHandComponent = ({name}) => {
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
 }
-const RightHandComponent=()=>{
-  return <h1 style={{backgroundColor:"red"}}>Right</h1>;
+const RightHandComponent = ({message}) => {
+  return <h1 style={{ backgroundColor: "red" }}>{message}</h1>;
 }
 function App() {
   return (
-    <SplitScreen
-    left={LeftHandComponent}
-    right={RightHandComponent}
-    />
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Dinith"/>
+      <RightHandComponent message="Full Stack developer"/>
+    </SplitScreen>
   );
 }
 
